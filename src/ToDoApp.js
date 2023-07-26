3326aa3c7f864c71080972636924e43c471ee03a
Omar Desouky <omaressamdesouky2274@gmail.com>
2023-07-26 10:17:10 +0300

    adding custom hook

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -1,45 +1,13 @@
-import { useState } from "react";
 import ToDoList from "./ToDoList";
 import AddToDoForm from "./AddToDoForm";
-import log from "./logging";
+import useToDOItemAndList from "./useToDoItemAndList";
 
 export default function ToDoApp() {
-  const [todoItem, setTodoItem] = useState("");
-  const [todoList, setTodoList] = useState([]);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
-
-  function handleAdd() {
-    if (todoItem !== "") {
-      setTodoList([
-        ...todoList,
-        { id: crypto.randomUUID(), text: todoItem, done: false },
-      ]);
-    }
-
-    setTodoItem("");
-  }
-
-  function handleDelete(id) {
-    const filteredList = todoList.filter((item) => {
-      return item.id !== id;
-    });
-
-    setTodoList(filteredList);
-  }
-
-  function handleCheck(id) {
-    log(id);
-    const filteredList = todoList.map((todoItem) => {
-      if (todoItem.id === id) {
-        return { ...todoItem, done: !todoItem.done };
-      }
-      return todoItem;
-    });
-    setTodoList([...filteredList]);
-  }
+  const [
+    todoItem,
+    todoList,
+    { handleSubmit, handleAdd, handleDelete, handleCheck, setTodoItem },
+  ] = useToDOItemAndList("", []);
 
   return (
     <>
---
+++ src/useToDoItemAndList.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import log from "./logging";
+
+export default function useToDOItemAndList(initialItem, initialList) {
+  const [todoItem, setTodoItem] = useState(initialItem);
+  const [todoList, setTodoList] = useState(initialList);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  function handleAdd() {
+    if (todoItem !== "") {
+      setTodoList([
+        ...todoList,
+        { id: crypto.randomUUID(), text: todoItem, done: false },
+      ]);
+    }
+
+    setTodoItem("");
+  }
+
+  function handleDelete(id) {
+    const filteredList = todoList.filter((item) => {
+      return item.id !== id;
+    });
+
+    setTodoList(filteredList);
+  }
+
+  function handleCheck(id) {
+    log(id);
+    const filteredList = todoList.map((todoItem) => {
+      if (todoItem.id === id) {
+        return { ...todoItem, done: !todoItem.done };
+      }
+      return todoItem;
+    });
+    setTodoList([...filteredList]);
+  }
+
+  return [
+    todoItem,
+    todoList,
+    {
+      handleSubmit,
+      handleAdd,
+      handleDelete,
+      handleCheck,
+      setTodoItem,
+    },
+  ];
+}
